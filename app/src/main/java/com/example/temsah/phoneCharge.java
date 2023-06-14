b2837591df80ca3ec108b1542a87e02fa018bd9b
SanazSobhanloo <cnzsg1@gmail.com>
2023-06-14 23:07:15 +0330

    Bindings

--- app/src/main/java/com/example/temsah/phoneCharge.java
+++ app/src/main/java/com/example/temsah/phoneCharge.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.temsah.databinding.ActivityAboutUsBinding;
+import com.example.temsah.databinding.ActivityPhoneChargeBinding;
+
 public class phoneCharge extends AppCompatActivity {
+    ActivityPhoneChargeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_charge);
+        binding= ActivityPhoneChargeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
