33de91e3dbc3f70b2513e82fa47bc1a155bc57b5
tinamhm <shatinaabbasi@gmail.com>
2023-06-15 21:14:37 +0430

    adding sub-sections to phone charge page

--- app/src/main/java/com/example/temsah/fixedLine.java
+++ app/src/main/java/com/example/temsah/fixedLine.java
@@ -3,10 +3,13 @@ package com.example.temsah;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.temsah.databinding.ActivityContactUsBinding;
 import com.example.temsah.databinding.ActivityFixedLineBinding;
 import com.example.temsah.databinding.ActivityMainBinding;
+
+import org.json.JSONObject;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -16,13 +19,29 @@ public class fixedLine extends AppCompatActivity {
     public static final MediaType JSON
             = MediaType.get("application/json;charset=utf-8");
     OkHttpClient client=new OkHttpClient();
-
+    
+    
+    String mid;
+    String fin;
+      
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fixed_line);
         binding= ActivityFixedLineBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.estelamBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number=binding.phone.getText().toString();
+                callAPI(number);
+            }
+        });
+
+    }
+
+    private void callAPI(String number) {
+        JSONObject object=new JSONObject();
 
     }
 }
--- app/src/main/java/com/example/temsah/phoneCharge.java
+++ app/src/main/java/com/example/temsah/phoneCharge.java
@@ -3,6 +3,7 @@ package com.example.temsah;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.temsah.databinding.ActivityPhoneChargeBinding;
 
@@ -14,13 +15,67 @@ public class phoneCharge extends AppCompatActivity {
     public static final MediaType JSON
             = MediaType.get("application/json;charset=utf-8");
     OkHttpClient client=new OkHttpClient();
-
+    //Boolean state=Boolean.FALSE;
+    Integer price;
+    Integer finalprice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_charge);
         binding=ActivityPhoneChargeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        binding.bwHamrahAval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.bwHamrahAval.setVisibility(View.INVISIBLE);
+                binding.hamrahAval.setVisibility(View.VISIBLE);
+            }
+        });
+        binding.bwIrancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.bwIrancel.setVisibility(View.INVISIBLE);
+                binding.irancel.setVisibility(View.VISIBLE);
 
+            }
+        });
+        binding.bwRightel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.bwRightel.setVisibility(View.INVISIBLE);
+                binding.rightel.setVisibility(View.VISIBLE);
+            }
+        });
+        binding.p10000.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                binding.priceBtn.setText("1000");
+                price=Integer.parseInt(binding.priceBtn.getText().toString());
+                finalprice=(price+200);
+                binding.showCharge.append(finalprice.toString());
+            }
+        });
+         binding.p20000.setOnClickListener(new View.OnClickListener() {
+               @Override
+                public void onClick(View view) {
+                   binding.priceBtn.setText("2000");
+                   price=Integer.parseInt(binding.priceBtn.getText().toString());
+                }
+         });
+         binding.p10000.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 binding.priceBtn.setText("5000");
+                 price=Integer.parseInt(binding.priceBtn.getText().toString());
+
+             }
+         });
+         binding.p100000.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 binding.priceBtn.setText("10000");
+                 price=Integer.parseInt(binding.priceBtn.getText().toString());
+             }
+         });
     }
 }
