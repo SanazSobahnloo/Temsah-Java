062af8a822764791be01c6f6981b45523d6cf6ba
tinamhm <shatinaabbasi@gmail.com>
2023-06-16 23:35:35 +0430

    fixing small problems on phonecharge

--- app/src/main/java/com/example/temsah/phoneCharge.java
+++ app/src/main/java/com/example/temsah/phoneCharge.java
@@ -2,6 +2,7 @@ package com.example.temsah;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
@@ -18,6 +19,7 @@ public class phoneCharge extends AppCompatActivity {
     //Boolean state=Boolean.FALSE;
     Integer price;
     Integer finalprice;
+    Integer maliat=300;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,11 @@ public class phoneCharge extends AppCompatActivity {
             public void onClick(View view) {
                 binding.bwHamrahAval.setVisibility(View.INVISIBLE);
                 binding.hamrahAval.setVisibility(View.VISIBLE);
+                binding.irancel.setVisibility(View.INVISIBLE);
+                binding.bwIrancel.setVisibility(View.VISIBLE);
+                binding.rightel.setVisibility(View.INVISIBLE);
+                binding.bwRightel.setVisibility(View.VISIBLE);
+                binding.phoneNumber.setBackgroundColor(Color.parseColor("#febe10"));
             }
         });
         binding.bwIrancel.setOnClickListener(new View.OnClickListener() {
@@ -36,7 +43,11 @@ public class phoneCharge extends AppCompatActivity {
             public void onClick(View view) {
                 binding.bwIrancel.setVisibility(View.INVISIBLE);
                 binding.irancel.setVisibility(View.VISIBLE);
-
+                binding.rightel.setVisibility(View.INVISIBLE);
+                binding.bwRightel.setVisibility(View.VISIBLE);
+                binding.hamrahAval.setVisibility(View.INVISIBLE);
+                binding.bwHamrahAval.setVisibility(View.VISIBLE);
+                binding.phoneNumber.setBackgroundColor(Color.parseColor("#54c5d0"));
             }
         });
         binding.bwRightel.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +55,12 @@ public class phoneCharge extends AppCompatActivity {
             public void onClick(View view) {
                 binding.bwRightel.setVisibility(View.INVISIBLE);
                 binding.rightel.setVisibility(View.VISIBLE);
+                binding.irancel.setVisibility(View.INVISIBLE);
+                binding.bwIrancel.setVisibility(View.VISIBLE);
+                binding.hamrahAval.setVisibility(View.INVISIBLE);
+                binding.bwHamrahAval.setVisibility(View.VISIBLE);
+                binding.phoneNumber.setBackgroundColor(Color.parseColor("#941063"));
+
             }
         });
         binding.p10000.setOnClickListener(new View.OnClickListener() {
@@ -51,8 +68,8 @@ public class phoneCharge extends AppCompatActivity {
             public void onClick(View view) {
                 binding.priceBtn.setText("1000");
                 price=Integer.parseInt(binding.priceBtn.getText().toString());
-                finalprice=(price+300);
-                binding.showCharge.append(finalprice.toString());
+                finalprice=(price+maliat);
+                binding.showCharge.setText(finalprice.toString());
             }
         });
          binding.p20000.setOnClickListener(new View.OnClickListener() {
@@ -60,13 +77,17 @@ public class phoneCharge extends AppCompatActivity {
                 public void onClick(View view) {
                    binding.priceBtn.setText("2000");
                    price=Integer.parseInt(binding.priceBtn.getText().toString());
+                   finalprice=(price+maliat);
+                   binding.showCharge.setText(finalprice.toString());
                 }
          });
-         binding.p10000.setOnClickListener(new View.OnClickListener() {
+         binding.p100000.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
-                 binding.priceBtn.setText("5000");
+                 binding.priceBtn.setText("10000");
                  price=Integer.parseInt(binding.priceBtn.getText().toString());
+                 finalprice=(price+maliat);
+                 binding.showCharge.setText(finalprice.toString());
 
              }
          });
@@ -75,6 +96,8 @@ public class phoneCharge extends AppCompatActivity {
              public void onClick(View view) {
                  binding.priceBtn.setText("10000");
                  price=Integer.parseInt(binding.priceBtn.getText().toString());
+                 finalprice=(price+maliat);
+                 binding.showCharge.setText(finalprice.toString());
              }
          });
     }
