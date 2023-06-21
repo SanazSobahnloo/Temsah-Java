8b0fb4b9a2fb67475041f979934d979d10e69226
SanazSobhanloo <cnzsg1@gmail.com>
2023-06-22 00:00:03 +0330

    splash screen final touch and tiny changes

--- app/src/main/java/com/example/temsah/MainActivity2.java
+++ app/src/main/java/com/example/temsah/MainActivity2.java
@@ -16,7 +16,7 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding=ActivityMain2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+//c
         Intent intentAboutUs = new Intent(this, aboutUs.class);
         Intent intentContactUs = new Intent(this, contactUs.class);
         Intent intentLogin = new Intent(this, login.class);
