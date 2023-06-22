2647b7ba2aed5d1971080fc2d864a62e08d041be
SanazSobhanloo <cnzsg1@gmail.com>
2023-06-22 21:59:28 +0330

    splash screen final touch and tiny changes n2

--- app/src/main/java/com/example/temsah/MainActivity2.java
+++ app/src/main/java/com/example/temsah/MainActivity2.java
@@ -23,6 +23,7 @@ public class MainActivity2 extends AppCompatActivity {
         Intent intentLogin = new Intent(this, login.class);
         Intent intentFixedLine = new Intent(this, fixedLine.class);
         Intent intentPhoneCharge = new Intent(this, phoneCharge.class);
+
         Intent intent=getIntent();
         String name=intent.getStringExtra("name");
         String phone=intent.getStringExtra("phone");
